c4684628f753f7c213d8f39c8cb51e0322614840
Júlia Fernandes Alves <juliafealves@gmail.com>
2018-09-11 15:15:44 -0300

    Adicionado testes no vetor

--- src/main/java/vetor/TestarVetor.java
+++ src/main/java/vetor/TestarVetor.java
@@ -2,9 +2,37 @@ package vetor;
 
 public class TestarVetor {
 
-	public static void main(String[] args) {
-		// preencha esse metodo com codigo para testar a classe vetor.
-		// À medida que voce evoluir no exercício o conteúdo deste mpetodo
-		// também será modificado.
-	}
+   public static void main(String[] args) {
+      Vetor vetor = new Vetor<Aluno>(3);
+      Aluno aluno = new Aluno("Julia", 9);
+      Aluno aluno1 = new Aluno("Ivana", 10);
+      Aluno aluno2 = new Aluno("Isabelle", 8);
+
+      System.out.println("Esta vazio? " + vetor.isVazio());
+      System.out.println("Existe? - Aluna Julia: " + vetor.procurar(aluno));
+
+      vetor.inserir(aluno);
+      System.out.println("Esta vazio? " + vetor.isVazio());
+
+      vetor.inserir(aluno1);
+      System.out.println("Esta cheio? " + vetor.isCheio());
+
+      vetor.inserir(aluno2);
+      System.out.println("Esta cheio? " + vetor.isCheio());
+
+      System.out.println("Existe? - Julia: " + vetor.procurar(aluno));
+      System.out.println("Existe? - Joana: " + vetor.procurar(new Aluno("Joana", 7)));
+
+      vetor.remover(aluno);
+      System.out.println(vetor.procurar(aluno));
+
+      vetor.setComparadorMaximo(new ComparatorMaximo());
+      vetor.setComparadorMinimo(new ComparatorMinimo());
+      Aluno alunoMaximo = (Aluno) vetor.maximo();
+      Aluno alunoMinimo = (Aluno) vetor.minimo();
+
+      System.out.println("Max: " + alunoMaximo.getNome());
+      System.out.println("Min: " + alunoMinimo.getNome());
+
+   }
 }
